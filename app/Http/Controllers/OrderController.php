83a3db465704d6acc659110b57df85be4c8112b5
muhammadfahrul <muhammadfahrul481@gmail.com>
2020-08-24 12:44:36 +0700

    rest-api-meser

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -99,8 +99,8 @@ class OrderController extends Controller
     {
         $this->validate($request, [
             'id' => 'required',
-            'products.quantity' => 'required',
-            'products.product_id' => 'required|exists:t_products,id'
+            // 'products.quantity' => 'required',
+            // 'products.product_id' => 'required|exists:t_products,id'
         ]);
 
         $products = $request->input('products');
@@ -126,8 +126,8 @@ class OrderController extends Controller
     {
         $this->validate($request, [
             'id' => 'required',
-            'products.quantity' => 'required',
-            'products.product_id' => 'required|exists:t_products,id'
+            // 'products.quantity' => 'required',
+            // 'products.product_id' => 'required|exists:t_products,id'
         ]);
         
         $order = Order::find($id);
