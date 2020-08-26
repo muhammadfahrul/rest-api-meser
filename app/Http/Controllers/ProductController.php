7227f6e3aa1fc3e86b3db61987dad80348e3521f
muhammadfahrul <muhammadfahrul481@gmail.com>
2020-08-26 16:53:43 +0700

    rest-api-meser

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -253,7 +253,7 @@ class ProductController extends Controller
     {
         $data = Product::find($id);
         if($data) {
-            Storage::disk('gcs')->delete('images/' . $data['data']['image']);
+            Storage::disk('gcs')->delete('images/' . isset($data['data']['image']));
             $data->delete();
 
             Log::info('Deleting product by id');
