de5ba0acee4e7e2a04e886fe27364eb481f26cf6
muhammadfahrul <muhammadfahrul481@gmail.com>
2020-08-26 11:34:56 +0700

    rest-api-meser

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -215,26 +215,24 @@ class PaymentController extends Controller
     
             $response = Http::withHeaders($http_header)->post($url, $transaction_req);
             $results = $response->json();
+            $data->transaction_id = 0;
+            $data->transaction_time = "";
+            $data->transaction_status = "created";
 
-            return response()->json($results);
-            // $data->transaction_id = 0;
-            // $data->transaction_time = "";
-            // $data->transaction_status = "created";
+            if ($data->save()){
+                Log::info('Adding payment');
 
-            // if ($data->save()){
-            //     Log::info('Adding payment');
-
-            //     return response()->json([
-            //         "message" => "Transaction with bank transfer method is successful",
-            //         "status" => true, 
-            //         "data" => $data
-            //     ], 200);
-            // }else {
-            //     return response()->json([
-            //         "status" => false,
-            //         "message" => "Data failed to save"
-            //     ], 401);
-            // }
+                return response()->json([
+                    "message" => "Transaction with bank transfer method is successful",
+                    "status" => true, 
+                    "data" => $results
+                ], 200);
+            }else {
+                return response()->json([
+                    "status" => false,
+                    "message" => "Data failed to save"
+                ], 401);
+            }
         }else {
             return response()->json([
                 "message" => "An unexpected error occurred",
