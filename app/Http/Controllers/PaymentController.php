1cdac99c32f74727c42c6eb06cc8836a1099ce23
muhammadfahrul <muhammadfahrul481@gmail.com>
2020-08-27 19:08:04 +0700

    rest-api-meser

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -182,23 +182,25 @@ class PaymentController extends Controller
                 'Accept' => 'application/json'
             ];
     
-            $response = Http::withHeaders($http_header)->post($url, $transaction_req);
-            $results = $response->json();
-            $data->transaction_id = $results["transaction_id"];
-            $data->transaction_time = $results["transaction_time"];
-            $data->transaction_status = $results["transaction_status"];
+            // $response = Http::withHeaders($http_header)->post($url, $transaction_req);
+            // $results = $response->json();
+            // $data->transaction_id = $results["transaction_id"];
+            // $data->transaction_time = $results["transaction_time"];
+            // $data->transaction_status = $results["transaction_status"];
             // $data->va_number = $results["va_numbers"]["va_number"];
 
-            if ($data->save()){
-                Log::info('Adding payment');
+            return response()->json($transaction_req);
 
-                return response()->json($results);
-            }else {
-                return response()->json([
-                    "message" => "Data failed to save",
-                    "status" => false
-                ], 401);
-            }
+            // if ($data->save()){
+            //     Log::info('Adding payment');
+
+            //     return response()->json($results);
+            // }else {
+            //     return response()->json([
+            //         "message" => "Data failed to save",
+            //         "status" => false
+            //     ], 401);
+            // }
         }else {
             return response()->json([
                 "message" => "An unexpected error occurred",
