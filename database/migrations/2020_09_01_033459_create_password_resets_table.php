2f6c8625812714e984a1e337e4a3b0438e920447
muhammadfahrul <muhammadfahrul481@gmail.com>
2020-09-01 10:36:04 +0700

    rest-api-meser

--- database/migrations/2020_09_01_033459_create_password_resets_table.php
+++ database/migrations/2020_09_01_033459_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('t_users', function (Blueprint $table) {
+        Schema::create('password_resets', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('t_users');
+        Schema::dropIfExists('password_resets');
     }
 }
