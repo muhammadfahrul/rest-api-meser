4ded86d297227f82f3cc9845ea85ad3e1db11594
muhammadfahrul <muhammadfahrul481@gmail.com>
2020-09-02 13:58:51 +0700

    rest-api-meser

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -201,6 +201,15 @@
           </tr>
           <!-- end button -->
 
+          {{-- <!-- start copy -->
+          <tr>
+            <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
+              <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
+              <p style="margin: 0;"><a href="https://sendgrid.com" target="_blank">https://same-link-as-button.url/xxx-xxx-xxxx</a></p>
+            </td>
+          </tr>
+          <!-- end copy --> --}}
+
           <!-- start copy -->
           <tr>
             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
