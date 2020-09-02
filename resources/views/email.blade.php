25537a1909907c7c1afbe6ba6a67e5289c4cd531
muhammadfahrul <muhammadfahrul481@gmail.com>
2020-09-02 10:44:02 +0700

    rest-api-meser

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -291,7 +291,9 @@
                             <table border="0" cellpadding="0" cellspacing="0">
                                 <tr>
                                     <td>
-                                        <p>Hi {{ $name }}</p>
+                                        @foreach ($data->name[0] as $item)
+                                        <p>Hi {{ $item }}</p>
+                                        @endforeach
                                         <p>Terimakasih telah mengirimkan email :)</p>
 
                                         <table border="0" cellpadding="0" cellspacing="0">
@@ -304,7 +306,9 @@
                                                             <td> <div class="container">
                                                                     <center><p>Pesanmu Yaitu :</p></center>
                                                                     <hr>
-                                                                    <center><p>{{ $message }}</p></center>
+                                                                    @foreach ($data->message[0] as $item)
+                                                                    <center><p>{{ $item }}</p></center>
+                                                                    @endforeach
                                                                     <hr>
                                                                     <center><p>Telah Disampaikan! <br> Terimakasih telah menghubungi kami!</p></center>
                                                                 </div>
